91b3f70ac20c42e680a78af4b59ee7616591733f
adeshkadambi <adeshkadambi@gmail.com>
2020-05-28 15:42:20 -0400

    assignment week 2

--- Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
+++ Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
@@ -1,15 +1,19 @@
 some_var = "false"
 another_var = "nil"
 
-if some_var == "pink elephant"
+case 
+when some_var == 'pink elephant' # fails some_var is "false" not "pink elephant"
   puts "Don't think about the pink elephant!"
-
-elsif another_var.nil?
+when another_var.nil? # fails because another_var is a string not nil type
   puts "Question mark in the method name?"
-
-elsif some_var == false
+when some_var == false # same as above
   puts "Looks like this one should execute"
-
 else
   puts "I guess nothing matched... But why?"
 end
+
+
+#.....
+
+#Finished in 0.00608 seconds (files took 0.17561 seconds to load)
+#5 examples, 0 failures
